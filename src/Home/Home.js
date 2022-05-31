beebafcd31ecb917e3095e2afd4b6c9cfc0b869c
Ibahmed1 <ib.ahmed1@outlook.com>
2022-05-31 19:34:18 +0100

    added in a pop up when you click the barcode scanner item (this will later contain the actual webcam barcode scanner hopefully). added in a tab selector and the state of which tab is selected is tracked

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../GlobalComponents/Header/Header";
-
+import { Paper, TextField, IconButton, Tooltip } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ScannerButton from "./components/ScannerButton";
+import TabSelector from "./components/TabSelector";
 
 export default function Home(props) {
+  const [tab, setTab] = useState("Product name");
+
+  function selectTab(selectedTab) {
+    setTab(selectedTab);
+  }
+
   return (
     <div>
       <Header />
+      <div className="home-wrapper">
+        <div className="tab-selector">
+          <TabSelector selectTab={selectTab} />
+        </div>
+        <Paper
+          sx={{ flex: 1, width: "20%", padding: 2 }}
+          elevation={3}
+          className="search-box"
+        >
+          <TextField variant="outlined" label="search"></TextField>
+          <Tooltip title="search">
+            <IconButton aria-label="search" color="primary">
+              <SearchIcon />
+            </IconButton>
+          </Tooltip>
+          <ScannerButton />
+        </Paper>
+      </div>
     </div>
   );
 }
---
+++ src/Home/components/ScannerButton.js
@@ -0,0 +1,55 @@
+import React, { useState, Fragment } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Tooltip, IconButton } from "@mui/material";
+import CropFreeIcon from "@mui/icons-material/CropFree";
+
+export default function ScannerButton() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Fragment>
+      <Tooltip title="open barcode scanner">
+        <IconButton
+          aria-label="barcode"
+          color="primary"
+          onClick={handleClickOpen}
+        >
+          <CropFreeIcon />
+        </IconButton>
+      </Tooltip>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Scan a barcode</DialogTitle>
+        <DialogContent>
+          <DialogContentText></DialogContentText>
+          <Box
+            noValidate
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              m: "auto",
+              width: "fit-content",
+            }}
+          ></Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
+  );
+}
