1327e4b4949ec4f9607fdbdaa237df5552c2023f
Dan Michell <dan.michell1000@gmail.com>
2022-06-07 18:02:49 +0100

    Date range fix

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -21,6 +21,8 @@ export default function Profile(props) {
   const networking = new Networking();
 
   useEffect(() => {
+    const startDate = new Date();
+    setFrom(startDate.setMonth(startDate.getMonth() - 1));
     async function getUserGoals() {
       const response = await networking.getUserGoals();
       setUserGoals(response);
@@ -118,10 +120,7 @@ export default function Profile(props) {
                 <p variant="subtitle2" sx={{ fontWeight: "600" }}>
                   From
                 </p>
-                <DatePicker
-                  selected={from}
-                  onChange={(date) => setFrom(date)}
-                />
+                <DatePicker selected={from} onChange={(date) => setFrom(date)} />
               </Box>
               <Box>
                 <p variant="subtitle2" sx={{ fontWeight: "600" }}>
