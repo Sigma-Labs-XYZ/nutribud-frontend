74fd3623e0ce53b8f449636932bf9f8a775f3031
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-06-07 16:45:31 +0100

    speech-detection components

---
+++ src/Home/components/SpeechDetection.js
@@ -0,0 +1,34 @@
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
+import IconButton from "@mui/material/IconButton";
+import MicIcon from "@mui/icons-material/Mic";
+import MicOffIcon from "@mui/icons-material/MicOff";
+import { Alert } from "@mui/material";
+import { useState } from "react";
+
+export default function SpeechDetection(props) {
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  const [isMicOn, setIsMicOn] = useState(false);
+  const appId = "1aa6b083-998a-424b-9ee4-c6246819b72f";
+  const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
+  SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
+  if (!browserSupportsSpeechRecognition) {
+    return <Alert>Browser doesn't support speech recognition.</Alert>;
+  }
+  function handleMicClick() {
+    setIsMicOn(!isMicOn);
+    if (isMicOn) {
+      props.updateTranscipt(transcript);
+      SpeechRecognition.stopListening();
+      resetTranscript();
+    } else {
+      SpeechRecognition.startListening({ continuous: true });
+    }
+  }
+  return (
+    <IconButton aria-label="mic" color="primary" onClick={handleMicClick}>
+      {isMicOn ? <MicIcon /> : <MicOffIcon />}
+    </IconButton>
+  );
+}
