963bf1f7596f015475496029c1dcb1d5c3b6d53f
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-06-08 10:14:18 +0100

    deleted unneccesery imports

--- src/Home/components/SpeechDetection.js
+++ src/Home/components/SpeechDetection.js
@@ -1,18 +1,12 @@
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-import { createSpeechlySpeechRecognition } from "@speechly/speech-recognition-polyfill";
-import { SpeechProvider } from "@speechly/react-client";
 import IconButton from "@mui/material/IconButton";
 import MicIcon from "@mui/icons-material/Mic";
 import MicOffIcon from "@mui/icons-material/MicOff";
 import { Alert } from "@mui/material";
-import { useState } from "react";
 
 export default function SpeechDetection(props) {
   const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
-  //   const appId = "1aa6b083-998a-424b-9ee4-c6246819b72f";
-  //   const SpeechlySpeechRecognition = createSpeechlySpeechRecognition(appId);
-  //   SpeechRecognition.applyPolyfill(SpeechlySpeechRecognition);
   if (!browserSupportsSpeechRecognition) {
     return <Alert>Browser doesn't support speech recognition.</Alert>;
   }
